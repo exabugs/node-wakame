aef6f7a3febca2dec9e5ff2dd5ae725d069dc954
exabugs <exabugs@gmail.com>
2014-04-21 17:47:55 +0900

    Modified test case.

--- test/mecab_test.js
+++ test/mecab_test.js
@@ -13,12 +13,15 @@ describe('MeCab', function () {
 
   it('MeCab', function (done) {
 
-    var process = MeCab.parse("今日は残業");
+    var process = MeCab.parse("今日は残業。明日も残業。");
 
-    var result = [];
+    var result = {};
 
     process.on('record', function (record, index) {
-      result.push(record);
+      if (record[1] === '名詞') {
+        var count = result[record[0]];
+        result[record[0]] = count ? ++count : 1;
+      }
     });
 
     process.on('error', function (error) {
@@ -26,13 +29,11 @@ describe('MeCab', function () {
     });
 
     process.on('end', function (count) {
-      should.equal(result[0][0], "今日");
-      should.equal(result[1][0], "は");
-      should.equal(result[2][0], "残業");
+      result.should.eql({ '今日': 1, '残業': 2, '明日': 1 });
       done(null, result, count);
     });
 
-  })
+  });
 
 });
 
