fb52d86dd3ac92c918bfaa8332025cf1bebca52a
exabugs <exabugs@gmail.com>
2014-04-21 16:41:18 +0900

    Modified test case.

---
+++ test/mecab_test.js
@@ -0,0 +1,38 @@
+/**
+ * MeCab
+ * @author exabugs@gmail.com
+ */
+
+"use strict";
+
+var should = require("should")
+  , MeCab = require('node-wakame').MeCab
+  ;
+
+describe('MeCab', function () {
+
+  it('MeCab', function (done) {
+
+    var process = MeCab.parse("今日は残業");
+
+    var result = [];
+
+    process.on('record', function (record, index) {
+      result.push(record);
+    });
+
+    process.on('error', function (error) {
+      done(error);
+    });
+
+    process.on('end', function (count) {
+      should.equal(result[0][0], "今日");
+      should.equal(result[1][0], "は");
+      should.equal(result[2][0], "残業");
+      done(null, result, count);
+    });
+
+  })
+
+});
+
