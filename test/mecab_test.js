a53c2a9609a52c98335ca8d8a22c553583f94653
exabugs <exabugs@gmail.com>
2014-04-22 11:53:12 +0900

    Add Test Case 2.

--- test/mecab_test.js
+++ test/mecab_test.js
@@ -6,14 +6,41 @@
 "use strict";
 
 var should = require("should")
-  , MeCab = require('../index.js')
+  , fs = require('fs')
+  , MeCab = require('../index')
   ;
 
 describe('MeCab', function () {
 
-  it('MeCab', function (done) {
+  it('Case 1 : String', function (done) {
 
-    var process = MeCab.parse("今日は残業。明日も残業。");
+    var input = "今日は残業。明日も残業。";
+    var process = MeCab.parse(input);
+
+    var result = {};
+
+    process.on('record', function (record, index) {
+      if (record[1] === '名詞') {
+        var count = result[record[0]];
+        result[record[0]] = count ? ++count : 1;
+      }
+    });
+
+    process.on('error', function (error) {
+      done(error);
+    });
+
+    process.on('end', function (count) {
+      result.should.eql({ '今日': 1, '残業': 2, '明日': 1 });
+      done(null, result, count);
+    });
+
+  });
+
+  it('Case 2: Stream', function (done) {
+
+    var input = fs.createReadStream('test/data.txt');
+    var process = MeCab.parse(input);
 
     var result = {};
 
