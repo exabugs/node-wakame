0af15ad40bfbb25fed48f493e117528e121a33f2
exabugs <exabugs@gmail.com>
2014-04-22 09:35:23 +0900

    Add lib dir.

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
 /*
- * MeCab
+ * node-wakame
  * @author exabugs@gmail.com
  */
 
-
-"use strict";
-
-var spawn = require('child_process').spawn
-  , csv = require('csv')
-  ;
-
-/**
- * parse
- * @param input string or stream
- * @returns {*|CursorStream|null}
- */
-exports.parse = function (input) {
-
-  var mecab = spawn('mecab');
-
-  var process = csv().from.stream(mecab.stdout, {delimiter: '\t', quote: null});
-
-  if ('string' === typeof input) {
-    mecab.stdin.write(input);
-    mecab.stdin.end();
-  } else {
-    input.pipe(mecab.stdin);
-  }
-
-  process.transform(function (record, index, next) {
-    if (1 < record.length) {
-      var result = [record[0]];
-      next(null, result.concat(record[1].split(',')));
-    } else {
-      next(null, record);
-    }
-  });
-
-  return process;
-};
-
+module.exports = require('./lib/mecab');
